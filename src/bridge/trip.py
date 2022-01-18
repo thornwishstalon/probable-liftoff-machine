58eb493dba3458ad2f8ba7b1ea20e0ee9b88ddcd
Fabian Pechstein <thornwishstalon@gmail.com>
2022-01-18 20:23:11 +0100

    [DRAFT] adding bridge state machine; draft event payload; add PUT to /floor endpoint

---
+++ src/bridge/trip.py
@@ -0,0 +1,100 @@
+import time
+
+
+class Trip:
+    def __init__(self, to):
+        self.to = to
+        self.time = time.time()
+
+
+class TripSchedule:
+
+    def __init__(self):
+        self.queue = {}
+        self.disk_size = 5 # we have only 5 floors ;)
+
+    def schedule_trip(self, trip):
+        self.queue[trip.to] = trip
+
+    def has_trip_scheduled(self):
+        return len(self.queue) > 0
+
+    def pop_trip(self, current_floor):
+        # queue is empty
+        if len(self.queue) == 0:
+            return None
+        if len(self.queue) == 1:
+            next_floor = list(self.queue.values())[0].to
+        else:
+            next_floor = self.SCAN(list(self.queue.keys()), current_floor, "down")
+
+        del (self.queue[next_floor])
+
+        return next_floor
+
+    def SCAN(self, arr, head, direction):
+        """
+        implementation of SCAN Algorithm,  source https://www.geeksforgeeks.org/scan-elevator-disk-scheduling-algorithms/
+        props to divyesh072019
+        """
+        seek_count = 0
+        down = []
+        up = []
+        seek_sequence = []
+
+        # Appending end values
+        # which has to be visited
+        # before reversing the direction
+        if direction == "down":
+            down.append(0)
+        elif direction == "up":
+            up.append(self.disk_size - 1)
+
+        for i in range(len(self.queue)):
+            if arr[i] < head:
+                down.append(arr[i])
+            if arr[i] > head:
+                up.append(arr[i])
+        # Sorting down and up vectors
+        down.sort()
+        up.sort()
+        # Run the while loop two times.
+        # one by one scanning up
+        # and down of the head
+        run = 2
+        while (run != 0):
+            if direction == "down":
+                for i in range(len(down) - 1, -1, -1):
+                    cur_track = down[i]
+                    # Appending current track to
+                    # seek sequence
+                    seek_sequence.append(cur_track)
+                    # Calculate absolute distance
+                    distance = abs(cur_track - head)
+                    # Increase the total count
+                    seek_count += distance
+                    # Accessed track is now the new head
+                    head = cur_track
+                direction = "up"
+            elif direction == "up":
+                for i in range(len(up)):
+                    cur_track = up[i]
+                    # Appending current track to seek
+                    # sequence
+                    seek_sequence.append(cur_track)
+                    # Calculate absolute distance
+                    distance = abs(cur_track - head)
+                    # Increase the total count
+                    seek_count += distance
+                    # Accessed track is now new head
+                    head = cur_track
+                direction = "down"
+            run -= 1
+
+        # print("Total number of seek operations =",
+        #       seek_count)
+        # print("Seek Sequence is")
+        # for i in range(len(seek_sequence)):
+        #     print(seek_sequence[i])
+
+        return seek_sequence[0] if seek_sequence[0] != 0 else seek_sequence[1]
--- src/bridge_server.py
+++ src/bridge_server.py
@@ -1,34 +1,97 @@
-from machine import Pin,SoftI2C, Timer, unique_id
+from machine import Pin, SoftI2C, Timer, unique_id
 import ubinascii
 import time
+import urandom
+
 from bridge.bridge import setup_bridge
+from bridge.trip import TripSchedule
 from common.credentials import Config
-from common.event import EVENT_UPDATE_FLOOR
+from common.event import EVENT_UPDATE_FLOOR, EVENT_PRE_TRIP_START, EVENT_TRIP_START, EVENT_POST_TRIP_END, EventPayload, \
+    EVENT_PRE_TRIP_END
 from module.liftoff_module import LiftoffModule
 from module.subscriber import SubscriberList
-import socket
+
+keys = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+
+class BridgeStateMachine:
+    READY = 0
+    PREPARE_TRIP = 1
+    EXECUTE_TRIP = 2
+    FINISH_TRIP = 3
 
 
 class DataState:
     current_floor = 0
+    doors = 0
 
-    def update_current_floor(self, message):        
+    def update_current_floor(self, message):
         self.current_floor = int(message)
         return True
 
+    def open_doors(self):
+        self.doors = 1
+
+    def close_doors(self):
+        self.doors = 0
+
 
 class BridgeServer(LiftoffModule):
-    state = DataState()
-        
+
+    def __init__(self, config, lcd=None):
+        super().__init__(config, lcd)
+        self.data_state = DataState()
+        self.schedule = TripSchedule()
+        self.state = BridgeStateMachine.READY
+        self.transaction_code = ""
+
+    def update_state(self, timer):
+        if self.state == BridgeStateMachine.READY:
+            if self.schedule.has_trip_scheduled():
+                self.state = BridgeStateMachine.PREPARE_TRIP
+                self.transaction_code = BridgeServer.generate_code()
+
+        elif self.state == BridgeStateMachine.PREPARE_TRIP:
+            if self.data_state.doors == 0:
+                self.mqtt.publish(EVENT_PRE_TRIP_START, EventPayload(self.transaction_code, {}).json)
+                self.data_state.close_doors()
+            else:
+                self.state = BridgeStateMachine.EXECUTE_TRIP
+
+        elif self.state == BridgeStateMachine.EXECUTE_TRIP:
+            self.mqtt.publish(
+                EVENT_TRIP_START,
+                EventPayload(self.transaction_code,
+                             {"next": self.schedule.pop_trip(self.data_state.current_floor)}).json
+            )
+
+        elif self.state == BridgeStateMachine.FINISH_TRIP:
+            self.data_state.open_doors()
+            self.mqtt.publish(
+                EVENT_POST_TRIP_END,
+                EventPayload(self.transaction_code, {}).json
+            )
+            self.state = BridgeStateMachine.READY
+
+    @classmethod
+    def generate_code(cls):
+        return ''.join((urandom.choice(keys) for _ in range(8)))
+
+    def arrive(self):
+        self.state = BridgeStateMachine.FINISH_TRIP
+
     @property
     def subscriber(self):
         subs = SubscriberList()
-        subs.register(EVENT_UPDATE_FLOOR, self.state.update_current_floor, 500)
+        subs.register(EVENT_UPDATE_FLOOR, self.data_state.update_current_floor, 500)
+        subs.register(EVENT_PRE_TRIP_END, self.arrive, 500)
+
         return subs
 
 
 time.sleep_ms(500)
 import ssd1306
+
 # display
 i2c_rst = Pin(16, Pin.OUT)
 i2c_rst.value(0)
@@ -39,19 +102,17 @@ i2c_sc1 = Pin(15, Pin.OUT, Pin.PULL_UP)
 i2c_sda = Pin(4, Pin.OUT, Pin.PULL_UP)
 
 i2c = SoftI2C(scl=i2c_sc1, sda=i2c_sda)
-lcd= ssd1306.SSD1306_I2C(128,64,i2c)
-
-
+lcd = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 # timers
 fetch_timer = Timer(0)
+state_timer = Timer(1)
 
 #
 client_id = ubinascii.hexlify(unique_id())
 config = Config(client_id).load()
 
-
-module = BridgeServer(config,lcd=lcd)
+module = BridgeServer(config, lcd=lcd)
 module.start()
 print('network setup done')
 
@@ -59,8 +120,7 @@ web = setup_bridge(module)
 
 ###### TIMERS
 fetch_timer.init(period=1000, mode=Timer.PERIODIC, callback=module.run)
+state_timer.init(period=500, mode=Timer.PERIODIC, callback=module.update_state)
 
 ## run server
 web.run(debug=True, host=module.host, port=80)
-
-
